78ce16f412c8446eec3e0db942f1982e9bcaedc4
Nicolas Loisy <nicolas.loisy@yahoo.fr>
2023-11-25 10:35:46 +0100

    Other Font

--- Meteo-Alert/App.tsx
+++ Meteo-Alert/App.tsx
@@ -12,7 +12,15 @@ const App = () => {
   useEffect(() => {
     async function loadFont() {
       await Font.loadAsync({
+        'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
+        'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+        'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+        'Inter-ExtraLight': require('./assets/fonts/Inter-ExtraLight.ttf'),
+        'Inter-Light': require('./assets/fonts/Inter-Light.ttf'),
+        'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
         'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+        'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+        'Inter-Thin': require('./assets/fonts/Inter-Thin.ttf'),
       });
       setFontLoaded(true);
     }
